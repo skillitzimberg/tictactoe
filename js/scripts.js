ba313ffb2592dc593ad85d7164fc680a39f2de30
Ralph Perdomo and Scott Bergler <ralph@>
2018-11-01 10:03:08 -0700

    add board messenger object to report move/game status to human players.

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,16 @@ function Game() {
   this.player2= new Player('O',this.board);
   this.active = this.player1;
 
-  this.board.build(); // a method of BOARD used to create an array of SQUAREs - positions on board
+  this.board.build();
 }
 
 Game.prototype.turnHandler = function(position) {
   console.log(this.active.symbol, 'is in play. wants to move to: ',position)
 
 
-  // This handles moves from the UI
-
   if (this.active == this.player1) {
 
     if (this.board.makeMove(this.active, position)){
-      $("#"+position).text(this.active.symbol)
 
       this.active = this.player2;
       return
@@ -26,32 +23,19 @@ Game.prototype.turnHandler = function(position) {
 
   if (this.active == this.player2) {
     if (this.board.makeMove(this.active, position)){
-      $("#"+position).text(this.active.symbol)
-      
       this.active = this.player1;
 
       return
     }
-
   }
-
-  console.log('move made', this.active.symbol," is now active")
-  // call makeMove
-  // if make move == true
-  // activeplayer not current
-
-
-
-  // console.log(position)
-
 }
+
 
 function Square() {
   this.mark;
 }
 
 function Board() {
-  this.type = 'text';
   this.allSquares = [];
   this.gameOver = false; // reports if Sqaures have been marked play player
 }
@@ -65,25 +49,22 @@ Board.prototype.build = function() {
 }
 
 Board.prototype.makeMove = function(activePlayer, toWhere) {
-  this.activePlayer = activePlayer;
-  this.toWhere = toWhere;
-
   if (!this.gameOver) {
-    if (!this.allSquares[this.toWhere].mark) {
-      this.allSquares[this.toWhere].mark = this.activePlayer.symbol;
+    if (!this.allSquares[toWhere].mark) {
+      this.allSquares[toWhere].mark = activePlayer.symbol;
 
-      this.redraw();
-
-      this.gameWon();
+      this.gameWon(activePlayer);
+      this.guiDraw(activePlayer.symbol,toWhere)
 
       return true
     }
+    this.messenger({msg: 'invalid_move', board: this.allSquares, player: activePlayer});
     return false
   }
-  console.log("GAME OVER!");
+  this.messenger({msg: 'game_end', board: this.allSquares, player: activePlayer});
 }
 
-Board.prototype.gameWon = function() {
+Board.prototype.gameWon = function(activePlayer) {
   var winningCombos = [
     [0,1,2],[3,4,5],[6,7,8], // horiz
     [0,3,6],[1,4,7],[2,5,8], // verts
@@ -97,31 +78,45 @@ Board.prototype.gameWon = function() {
 
     if ((first  && second  && third) && (first === second && second === third && first === third)) {
 
-      console.log("match found! player ", this.allSquares[combo[0]].mark, "won!")
-
       this.gameOver = true;
-      return this.allSquares[combo[0]].mark
-
+      this.messenger({msg: 'game_win', board: combo, player: activePlayer});
+      // don't know why below was added—commenting out instead
+      // return this.allSquares[combo[0]].mark
     }}).bind(this));
 }
 
-Board.prototype.redraw = function() {
-  // strictly for text-based game
-  var ticTacToe = ["☐","☐","☐","☐","☐","☐","☐","☐","☐"];
+Board.prototype.guiDraw = function(symbol,toWhere) {
+  $("#"+toWhere).text(symbol)
 
-
-  for(var i=0; i<this.allSquares.length; i++) {
-    if (this.allSquares[i].mark) {
-      ticTacToe[i]=this.allSquares[i].mark
-    }
-
+  if (symbol === 'X') {
+    $("#player1, #player2").removeClass();
+    $("#player2").addClass("active");
   }
 
-  var toeBoard = ticTacToe[0]+ticTacToe[1]+ticTacToe[2]+"\n"+ticTacToe[3]+ticTacToe[4]+ticTacToe[5]+"\n"+ticTacToe[6]+ticTacToe[7]+ticTacToe[8]
-
-  console.log(toeBoard)
+  if (symbol === 'O') {
+    $("#player1, #player2").removeClass();
+    $("#player1").addClass("active");
+  }
 
 }
+
+Board.prototype.messenger = function(msg) {
+  console.log(msg)
+}
+
+// Board.prototype.redraw = function() {
+//   // text-based game
+//   var ticTacToe = ["☐","☐","☐","☐","☐","☐","☐","☐","☐"];
+//
+//   for(var i=0; i<this.allSquares.length; i++) {
+//     if (this.allSquares[i].mark) {
+//       ticTacToe[i]=this.allSquares[i].mark;
+//     }
+//
+//   }
+//   var toeBoard = ticTacToe[0]+ticTacToe[1]+ticTacToe[2]+"\n"+ticTacToe[3]+ticTacToe[4]+ticTacToe[5]+"\n"+ticTacToe[6]+ticTacToe[7]+ticTacToe[8];
+//   console.log(toeBoard);
+// }
 
 function Player(symbol, board) {
   this.symbol = symbol;
@@ -130,24 +125,13 @@ function Player(symbol, board) {
 }
 
 Player.prototype.move = function(position) {
-
   return this.board.makeMove(this, position)
-
-  // console.log( this.board.makeMove(this,position) )
-
 
 }
 
-// instantiate a new tic-tac-toe game below
-var game1 = new Game();
-
-
-
-
-// game1.player1.move(0)
-
-
 $(document).ready(function() {
+  // instantiate a new tic-tac-toe game below
+  var game1 = new Game();
   $("table#board").on("click", "td", function() {
     game1.turnHandler(this.id);
 
